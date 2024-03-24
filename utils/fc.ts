80f604721cf9216dfc182255d4c7d511b992ae6d
Bleyle <Bennybleylle09@gmail.com>
2024-03-24 14:27:07 +0300

    Delete utils directory

--- utils/fc.ts
+++
@@ -1,6 +0,0 @@
-export const getConnectedAddressForUser = async (fid: number) => {
-    const res = await fetch(`https://hub.pinata.cloud/v1/verificationsByFid?fid=${fid}`)
-    const json = await res.json();
-    const address = json.messages[0].data.verificationAddAddressBody.address
-    return address
-  }
